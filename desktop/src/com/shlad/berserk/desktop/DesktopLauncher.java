77c31b4e0e87bc4ab5dd5f614c62b30c831780ef
Christos Psimadas <chrispsimadas123@gmail.com>
2022-03-24 21:57:36 -0400

    Map WIP added

--- desktop/src/com/shlad/berserk/desktop/DesktopLauncher.java
+++ desktop/src/com/shlad/berserk/desktop/DesktopLauncher.java
@@ -10,9 +10,10 @@ public class DesktopLauncher
 	public static void main (String[] arg)
 	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		
-		config.height = 416;
-		config.width = 800;
+
+		//Aspect ratio -> 25x13
+		config.height = 585;
+		config.width = 1125;
 		
 		config.title = "Jump God";
 		config.addIcon("icon/icon.png", Files.FileType.Internal);
